75c8fe1e37ea3a14463cad18aea54572802e1412
Micorksen <hello@micorksen.eu>
2022-12-24 23:04:16 +0100

    Add requirements check + security.

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -19,17 +19,36 @@ class ServiceProvider extends IServiceProvider
   ];
 
   /**
+   * Check that the requirements are configured.
+   *
+   * @return bool
+   */
+  private function requirements(): bool
+  {
+    $casTicketPrefix = env('CAS_TICKET_PREFIX');
+    $providerName = env('OAUTH_PROVIDER');
+    $clientId = env('OAUTH_CLIENT_ID');
+    $clientSecret = env('OAUTH_CLIENT_SECRET');
+    $redirect = url('/oauth/callback');
+
+    return !$casTicketPrefix || !$providerName || !$clientId || !$clientSecret || !$redirect;
+  }
+
+  /**
    * Register any others events for your application.
    *
    * @return void
    */
   public function boot()
   {
-    $providerName = ucfirst(env('OAUTH_PROVIDER'));
-    config()->set('services.' . env('OAUTH_PROVIDER'), [
-      'client_id' => env('OAUTH_CLIENT_ID'),
-      'client_secret' => env('OAUTH_CLIENT_SECRET'),
-      'redirect' => url('/oauth/callback')
+    if (!$this->requirements()) {
+      return;
+    }
+
+    config()->set('services.' . $providerName, [
+      'client_id' => $clientId,
+      'client_secret' => $clientSecret,
+      'redirect' => $redirect
     ]);
 
     Event::listen(\SocialiteProviders\Manager\SocialiteWasCalled::class, "\SocialiteProviders\\$providerName\\$providerName" . "ExtendSocialite@handle");
@@ -42,6 +61,10 @@ class ServiceProvider extends IServiceProvider
    */
   public function register()
   {
+    if (!$this->requirements()) {
+      return;
+    }
+
     $this->app->register('SocialiteProviders\Manager\ServiceProvider');
     $this->loadRoutesFrom(__DIR__ . '/../resources/routes.php');
     $this->loadViewsFrom(__DIR__ . '/../resources/views', 'cas-oauth');
