e9bbb1dafafcf25e1dbc038ea712f3539aa050e6
Matteo C <hello@micorksen.eu>
2023-07-09 17:35:21 +0200

    Update ticket.blade.php

--- resources/views/ticket.blade.php
+++ resources/views/ticket.blade.php
@@ -2,19 +2,21 @@
   @if (isset($authenticationSuccess))
     <cas:authenticationSuccess>
       <cas:user>{{ $authenticationSuccess['user'] }}</cas:user>
-      <cas:attributes>
-        @foreach ($authenticationSuccess['attributes'] as $key => $value)
-          @if (is_array($value))
-            @foreach ($value as $value2)
-              <cas:{{ $key }}>{{ $value2 }}</cas:{{ $key }}>
-            @endforeach
-          @else
-            <cas:{{ $key }}>{{ $value }}</cas:{{ $key }}>
-          @endif
-        @endforeach
-      </cas:attributes>
+	  @if (isset($authenticationSuccess['attributes']))
+        <cas:attributes>
+          @foreach ($authenticationSuccess['attributes'] as $key => $value)
+            @if (is_array($value))
+              @foreach ($value as $value2)
+                <cas:{{ $key }}>{{ $value2 }}</cas:{{ $key }}>
+              @endforeach
+            @else
+              <cas:{{ $key }}>{{ $value }}</cas:{{ $key }}>
+            @endif
+          @endforeach
+        </cas:attributes>
+	  @endif
     </cas:authenticationSuccess>
   @else
-    <cas:authenticationFailure code='{{ $authenticationFailure['code'] }}'>{{ $authenticationFailure['description'] }}</cas:authenticationFailure>
+    <cas:authenticationFailure code="{{ $authenticationFailure['code'] }}">{{ $authenticationFailure['description'] }}</cas:authenticationFailure>
   @endif
 </cas:serviceResponse>
